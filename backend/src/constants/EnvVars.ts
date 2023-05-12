aa78b44368a89a03987f784db336f461cf3e48df
Udoka <udochukwukaonyela@gmail.com>
2023-05-12 23:42:31 +0100

    feat: configured environmental vairables

--- backend/src/constants/EnvVars.ts
+++ backend/src/constants/EnvVars.ts
@@ -25,4 +25,5 @@ export default {
     Secret: (process.env.JWT_SECRET ??  ''),
     Exp: (process.env.COOKIE_EXP ?? ''), // exp at the same time as the cookie
   },
+  MONGO_URI: (process.env.MONGO_URI ?? ''),
 } as const;
