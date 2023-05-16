87b0163152a8281184e599203be3794eb2c09c3c
Udoka <udochukwukaonyela@gmail.com>
2023-05-16 10:00:52 +0100

    chore: added new routes

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,8 +1,11 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+
+import { Routes } from '@angular/router';
 import { HomeComponent } from "./home/home.component";
 import { ProductDetailsComponent } from "./product-details/product-details.component";
 import { CartComponent } from "./cart/cart.component";
+import { UserComponent } from "./user/user.component";
+import { LoginComponent } from "./login/login.component";
+import { RegisterComponent } from "./register/register.component";
 
 const routes: Routes = [
   {
@@ -19,6 +22,20 @@ const routes: Routes = [
     path: 'cart',
     component: CartComponent,
     title: 'cart'
+  },
+  {
+    path: 'user',
+    component: UserComponent,
+    title: 'user'
+  },
+  {
+    path: 'login',
+    component: LoginComponent,
+    title: 'login'
+  },
+  {
+    path: 'register',
+    component: RegisterComponent,
   }
 ];
 
