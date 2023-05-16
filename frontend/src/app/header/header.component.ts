31e580adfbdf1e1c0dcc2657ef7398282e71dc8a
Udoka <udochukwukaonyela@gmail.com>
2023-05-16 01:07:56 +0100

    feat: subscribed to dervice and added search feature

--- frontend/src/app/header/header.component.ts
+++ frontend/src/app/header/header.component.ts
@@ -1,7 +1,8 @@
 import { CommonModule, NgIf } from '@angular/common';
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { FormControl, ReactiveFormsModule } from '@angular/forms';
 import { RouterLink, RouterOutlet } from '@angular/router';
+import { CartService } from "../cart.service";
 @Component({
   selector: 'app-header',
   standalone: true,
@@ -13,20 +14,21 @@ import { RouterLink, RouterOutlet } from '@angular/router';
       </a>
       <div class="d-flex fs-4" style="flex-basis: 50%;width: 100%;gap: 20px;">
         <form class="d-flex" role="search" style="flex-basis: 100%">
-          <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-          <button class="btn btn-outline-success" type="submit">Search</button>
+          <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search" [formControl]="search">
+          <button class="btn btn-outline-success" type="button" (click)="onSearch($event)">Search</button>
         </form>
         <div class="d-flex gap-4 fs-4">
       <i class="bi bi-person-circle"></i>
-      <div type="button" class="position-relative">
+      <a routerLink="/cart" class="position-relative">
         <i class="bi bi-cart"></i>
         <span
           *ngIf="cartItems > 0"
-          class="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger text"
+          class="position-absolute start-100 translate-middle badge rounded-pill bg-danger text"
+          style="font-size: 0.6rem;top: 9px;"
         >
           {{ cartItems }}
         </span>
-      </div>
+      </a>
       </div>
       </div>
     </nav>
@@ -37,11 +39,20 @@ export class HeaderComponent implements OnInit {
   value = 'Clear me';
   public cartItems: number = 0;
   public searchControl = new FormControl();
-  constructor() { }
-  onSubmit() {
-    console.log(this.searchControl.value);
-  }
+  @Output() private searcher!: EventEmitter<string>;
+  public search = new FormControl('');
+  constructor(private cartService: CartService) {
+    this.searcher = new EventEmitter<string>();
+   }
   ngOnInit(): void {
-    this.cartItems = 0;
+    this.getCartItems()
+  }
+  getCartItems(): void {
+    console.log(this.cartService.getItems().pipe().subscribe(items => {
+      this.cartItems = items.length;
+    }))
+  }
+  onSearch(event: any) {
+    this.searcher.emit(this.search.value ?? '');
   }
 }
