faa3296da9ff0deff7984f9a298d3631d6932ac1
Udoka <udochukwukaonyela@gmail.com>
2023-05-16 10:00:40 +0100

    feat: added content for user page

--- frontend/src/app/user/user.component.ts
+++ frontend/src/app/user/user.component.ts
@@ -1,13 +1,24 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HeaderComponent } from "../header/header.component";
+import { RouterLink, RouterOutlet } from "@angular/router";
 
 @Component({
   selector: 'app-user',
   standalone: true,
-  imports: [CommonModule],
-  templateUrl: './user.component.html',
+  imports: [RouterLink, RouterOutlet,CommonModule, HeaderComponent],
+  template: `
+  <app-header></app-header>
+    <div class="container-fluid d-flex align-items-center justify-content-center" style="min-height: 80vh;">
+    <div class="d-flex gap-5">
+      <a routerLink="/register" type="button" class="btn btn-outline-primary">Register</a>
+      <a routerLink="/login" type="button" class="btn btn-primary">Login</a>
+      </div>
+    </div>
+  `,
   styleUrls: ['./user.component.scss']
 })
 export class UserComponent {
+  constructor() { }
 
 }
