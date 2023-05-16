1166238cb0067f7556309ee8b5cca0e87b6d22b9
Udoka <udochukwukaonyela@gmail.com>
2023-05-16 09:58:10 +0100

    feat: created component for register page

---
+++ frontend/src/app/register/register.component.ts
@@ -0,0 +1,81 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {
+  ReactiveFormsModule,
+  FormGroup,
+  FormControl,
+  Validators,
+} from '@angular/forms';
+
+@Component({
+  selector: 'app-register',
+  standalone: true,
+  imports: [CommonModule, ReactiveFormsModule],
+  template: `
+    <div
+      class="container-fluid row align-items-center justify-content-center p-4"
+      style="min-height: 100vh;"
+    >
+      <form
+        class="d-flex flex-column gap-3 col-12 col-sm-6 col-md-4"
+        [formGroup]="registrationForm"
+        (ngSubmit)="onSubmit()"
+      >
+        <div>
+          <label for="userName" class="mb-1">userName</label>
+          <input
+            type="name"
+            class="form-control"
+            id="inputName"
+            placeholder="userName"
+            formControlName="userName"
+          />
+        </div>
+        <div>
+          <label for="inputEmail" class="mb-1">Email</label>
+          <input
+            type="email"
+            class="form-control"
+            id="inputEmail"
+            placeholder="Email"
+            formControlName="email"
+          />
+        </div>
+        <div>
+          <label for="inputPassword" class="mb-1">Password</label>
+          <input
+            type="password"
+            class="form-control"
+            id="inputPassword"
+            placeholder="Password"
+            formControlName="password"
+          />
+        </div>
+        <button
+          type="submit"
+          class="btn btn-primary mb-3"
+          [disabled]="!registrationForm.valid"
+        >
+          Register
+        </button>
+      </form>
+    </div>
+  `,
+  styleUrls: ['./register.component.scss'],
+})
+export class RegisterComponent {
+  public registrationForm!: FormGroup;
+  constructor() {
+    this.createForm();
+  }
+  createForm() {
+    this.registrationForm = new FormGroup({
+      userName: new FormControl('', Validators.required),
+      email: new FormControl('', [Validators.required, Validators.email]),
+      password: new FormControl('', [Validators.required, Validators.minLength(6)]),
+    });
+  }
+  onSubmit() {
+    console.log(this.registrationForm);
+  }
+}
