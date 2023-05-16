0e8967e818ef9f57dd5b8ba743e0e5475b1ecfd4
Udoka <udochukwukaonyela@gmail.com>
2023-05-16 20:39:35 +0100

    feat: added login method to user service

--- backend/src/services/UserService.ts
+++ backend/src/services/UserService.ts
@@ -3,11 +3,12 @@
 import User, { IUser } from '@src/models/User';
 import { RouteError } from '@src/other/classes';
 import HttpStatusCodes from '@src/constants/HttpStatusCodes';
+import PwdUtil from "@src/util/PwdUtil";
 
 // **** Variables **** //
 
 export const USER_NOT_FOUND_ERR = 'User not found';
-
+export const INCORECT_PASSWORD = 'Incorrect password';
 // **** Functions **** //
 
 async function register(user: IUser) {
@@ -15,31 +16,53 @@ async function register(user: IUser) {
   await userObj.save();
   return userObj;
 }
-
+async function login(email: string, password: string) {
+  const user: IUser | null = await User.findOne({ email });
+  if (!user) {
+    throw new RouteError(HttpStatusCodes.NOT_FOUND, USER_NOT_FOUND_ERR);
+  }
+  const isPasswordCorrect = await PwdUtil.compare(password, user.hashedPassword as string);
+  if (!isPasswordCorrect) {
+    throw new RouteError(HttpStatusCodes.UNAUTHORIZED, INCORECT_PASSWORD);
+  }
+  return user;
+}
+async function getUserById(id: IUser['_id']) {
+  const user: IUser[] = await User.findById(id).populate("history");
+  if (!user) {
+    throw new RouteError(HttpStatusCodes.NOT_FOUND, USER_NOT_FOUND_ERR);
+  }
+  return user;
+}
 async function getHistory(id: IUser['_id']) {
-  const user: IUser[] | null = await User.findById(id).populate(
-    'history.product',
-  );
-  if (!user || user.length < 1) {
+  const user: IUser[] = await User.findById(id).populate("history");
+  if (!user) {
     throw new RouteError(HttpStatusCodes.NOT_FOUND, USER_NOT_FOUND_ERR);
   }
   return user;
 }
 async function addHistory(id: IUser['_id'], product: string) {
-  const user: IUser | null = await User.findByIdAndUpdate(
-    id,
-    {
-      $push: { history: product },
-    },
-    { new: true },
-  );
-  if (!user) {
-    throw new RouteError(HttpStatusCodes.NOT_FOUND, USER_NOT_FOUND_ERR);
+  const check = await User.findById(id);
+  if (check.history.includes(product)) {
+    return check.history
+  } else {
+    const user: IUser | null = await User.findByIdAndUpdate(
+      id,
+      {
+        $push: { history: product },
+      },
+      { new: true },
+    );
+    if (!user) {
+      throw new RouteError(HttpStatusCodes.NOT_FOUND, USER_NOT_FOUND_ERR);
+    }
+    return user.history;
   }
-  return user.history;
 }
 export default {
   register,
+  login,
   getHistory,
   addHistory,
+  getUserById,
 } as const;
