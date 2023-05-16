4ea3d575fd01d5a93f9eefea669862eccb2d72fd
Udoka <udochukwukaonyela@gmail.com>
2023-05-16 20:40:27 +0100

    feat: changed local type

--- backend/src/routes/types/express/misc.ts
+++ backend/src/routes/types/express/misc.ts
@@ -1,6 +1,5 @@
 import * as e from 'express';
 
-import { ISessionUser } from '@src/models/User';
 
 
 // **** Express **** //
@@ -11,6 +10,6 @@ export interface IReq<T = void> extends e.Request {
 
 export interface IRes extends e.Response {
   locals: {
-    sessionUser?: ISessionUser;
+    sessionUser?: { userId: string};
   };
 }
