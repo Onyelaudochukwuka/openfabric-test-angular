5e4ead7b307f8a686957e60c47c6f86285d9954e
Udoka <udochukwukaonyela@gmail.com>
2023-05-16 21:25:06 +0100

    fixed logo

--- frontend/src/app/header/header.component.ts
+++ frontend/src/app/header/header.component.ts
@@ -35,6 +35,7 @@ import { ApiService } from "../api.service";
       </div>
     </nav>
   `,
+  styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent implements OnInit {
   value = 'Clear me';
