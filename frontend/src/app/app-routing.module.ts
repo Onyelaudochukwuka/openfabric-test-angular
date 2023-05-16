2f6e45a43a2aab6de8725956d398e66295dad572
Udoka <udochukwukaonyela@gmail.com>
2023-05-16 01:07:12 +0100

    feat: added routes

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,12 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from "./home/home.component";
+import { ProductDetailsComponent } from "./product-details/product-details.component";
+import { CartComponent } from "./cart/cart.component";
 
 const routes: Routes = [
   {
     path: '',
     component: HomeComponent,
     title: 'Home page'
+  },
+  {
+    path: 'product/:id',
+    component: ProductDetailsComponent,
+    title: 'Product details'
+  },
+  {
+    path: 'cart',
+    component: CartComponent,
+    title: 'cart'
   }
 ];
 
