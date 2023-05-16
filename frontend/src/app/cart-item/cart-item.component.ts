a0484bd99041bf9ea0d50cb1ea264ec975eb08c6
Udoka <udochukwukaonyela@gmail.com>
2023-05-16 01:06:43 +0100

    feat: optimized image and added quantity element

--- frontend/src/app/cart-item/cart-item.component.ts
+++ frontend/src/app/cart-item/cart-item.component.ts
@@ -1,21 +1,26 @@
 import { Component, Input } from '@angular/core';
-import { CommonModule, NgFor } from '@angular/common';
+import { CommonModule, NgFor, NgOptimizedImage } from '@angular/common';
 import { Cart } from '../cart';
-import { CartService } from "../cart.service";
-
+import { CartService } from '../cart.service';
+import { RouterLink, RouterOutlet } from '@angular/router';
 @Component({
   selector: 'app-cart-item',
   standalone: true,
-  imports: [NgFor, CommonModule],
+  imports: [NgFor, CommonModule, RouterLink, RouterOutlet, NgOptimizedImage],
   template: `
     <div class="card" style="width: fit-content;">
+    <a [routerLink]="['/',cartItem._id]">
       <img
         class="card-img-top"
-        src="https://ng.jumia.is/unsafe/fit-in/300x300/filters:fill(white)/product/64/986649/1.jpg?6655"
-        class="w-full"
+        ngSrc="https://ng.jumia.is/unsafe/fit-in/300x300/filters:fill(white)/product/64/986649/1.jpg?6655"
+        class="w-100 h-auto"
+        priority
+        width="300"
+        height="300"
       />
+      </a>
       <div class="card-body">
-        <h6 class="card-title fw-bold fs-5">{{ cartItem.name }}</h6>
+        <a [routerLink]="['/product',cartItem._id]"><h6 class="card-title fw-bold fs-5">{{ cartItem.name }}</h6></a>
         <span class="card-text">$ {{ cartItem.price || 0 }}</span>
         <div class="d-flex flex-column">
           <small class="card-text">{{ getRating(cartItem.rating) || 0 }}</small>
@@ -25,8 +30,19 @@ import { CartService } from "../cart.service";
             ></span>
           </div>
         </div>
-        <button class="btn btn-primary fs-6 mt-3" (click)="addToCart()">Add to Cart</button>
-        <button class="btn btn-primary fs-6 mt-3" (click)="addToCart()">view products</button>
+        <div *ngIf="quantity" class="d-flex gap-2">
+          +
+          <input [value]="cartItem.quantity" class="card-text" disabled/>
+          -
+        </div>                  
+        <div class="d-flex gap-5">
+          <button class="btn btn-primary fs-6 mt-3" (click)="addToCart()">
+            Add to Cart
+          </button>
+          <a [routerLink]="['/product',cartItem._id]" class="btn btn-info text-white fs-6 mt-3">
+            View
+          </a>
+        </div>
       </div>
     </div>
   `,
@@ -35,6 +51,7 @@ import { CartService } from "../cart.service";
 export class CartItemComponent {
   public rating!: number;
   @Input() public cartItem!: Cart;
+  @Input() public quantity!: boolean;
   constructor(private cartService: CartService) {}
   getRating(ratingArr: number[] | undefined): number {
     let rating = 0;
@@ -45,6 +62,6 @@ export class CartItemComponent {
   }
   addToCart(): void {
     this.cartService.addToCart(this.cartItem);
-    console.log('ADDed')
+    console.log('ADDed');
   }
 }
